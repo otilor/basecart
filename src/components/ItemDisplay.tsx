70ca2805111b6e6a65783f488ed1b4630a6d6374
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2023-09-24 12:45:22 +0100

    remove itemdisplay.tsx

--- src/components/ItemDisplay.tsx
+++ src/components/ItemDisplay.tsx
@@ -1,10 +1,10 @@
-import { Item } from '../data/items';
-import { Button } from 'baseui/button';
+import {Item} from '../data/items';
+import {Button} from 'baseui/button';
 
-import { FlexGridItem } from 'baseui/flex-grid';
-import { Card, StyledBody, StyledAction } from 'baseui/card';
-import { Plus } from 'baseui/icon';
-import { BlockProps } from 'baseui/block';
+import {FlexGridItem, FlexGrid} from 'baseui/flex-grid';
+import {Card, StyledBody, StyledAction} from 'baseui/card';
+import {Plus} from 'baseui/icon';
+import {Block, BlockProps} from 'baseui/block';
 
 interface Props {
   items: Item[];
@@ -17,31 +17,37 @@ const itemProps: BlockProps = {
 
 function ItemDisplay({items, addToCart}: Props) {
   return (
-    <div>
-      {items.map((element, index) => (
-        <FlexGridItem key={index} {...itemProps}>
-          <Card
-            overrides={{Root: {style: {width: '328px'}}}}
-            headerImage={element.image}
-            title={element.name}
-          >
-            <StyledBody>
-              Proin ut dui sed metus pharetra hend rerit vel non mi. Nulla
-              ornare faucibus ex, non facilisis nisl.
-            </StyledBody>
-            <StyledAction>
-              <Button
-                startEnhancer={Plus}
-                onClick={() => addToCart(element)}
-                overrides={{BaseButton: {style: {width: '100%'}}}}
-              >
-                Add to Cart
-              </Button>
-            </StyledAction>
-          </Card>
-        </FlexGridItem>
-      ))}
-    </div>
+    <>
+      <FlexGrid
+        flexGridColumnCount={[1, 2, 4, 8]}
+        flexGridColumnGap="scale800"
+        flexGridRowGap="scale800"
+      >
+        {items.map((element, index) => (
+          <FlexGridItem key={index} {...itemProps}>
+            <Card
+              overrides={{Root: {style: {width: '328px'}}}}
+              headerImage={element.image}
+              title={element.name}
+            >
+              <StyledBody>
+                Proin ut dui sed metus pharetra hend rerit vel non mi. Nulla
+                ornare faucibus ex, non facilisis nisl.
+              </StyledBody>
+              <StyledAction>
+                <Button
+                  startEnhancer={Plus}
+                  onClick={() => addToCart(element)}
+                  overrides={{BaseButton: {style: {width: '100%'}}}}
+                >
+                  Add to Cart
+                </Button>
+              </StyledAction>
+            </Card>
+          </FlexGridItem>
+        ))}
+      </FlexGrid>
+    </>
   );
 }
 
