befe3056c4783e2cb9f813f18e20f51962ea73e5
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2023-09-24 12:45:38 +0100

    update items

--- src/data/items.ts
+++ src/data/items.ts
@@ -23,6 +23,54 @@ const quantites: Item[] = [
         'https://www.instacart.com/image-server/591x591/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_a21fe8c4-88b3-44b5-ba37-dd49ebed6014.jpg',
       quantity: 0,
     },
+    {
+        id: 3,
+        name: 'Onions',
+        price: 0.56,
+        image:
+        'https://www.instacart.com/image-server/591x591/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_d7dcd978-645a-4e93-a073-a2dcba9be774.jpg',
+        quantity: 0,
+      },
+      {
+        id: 4,
+        name: 'Onions',
+        price: 0.56,
+        image:
+        'https://www.instacart.com/image-server/591x591/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_18850fc6-5466-4c06-9d84-623509ffe430.jpg',
+        quantity: 0,
+      },
+      {
+        id: 5,
+        name: 'Onions',
+        price: 0.56,
+        image:
+        'https://www.instacart.com/image-server/591x591/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_06c8d88f-3fea-4139-96ba-db2b41ac51d1.jpg',
+        quantity: 0,
+      },
+      {
+        id: 6,
+        name: 'Onions',
+        price: 0.56,
+        image:
+        'https://www.instacart.com/image-server/591x591/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_8ee9d1a8-988f-4d07-99be-57fb05a68767.jpg',
+        quantity: 0,
+      },
+      {
+        id: 7,
+        name: 'Onions',
+        price: 0.56,
+        image:
+        'https://www.instacart.com/image-server/394x394/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_4453585c-c158-438d-bae5-980973efc462.jpg',
+        quantity: 0,
+      },
+      {
+        id: 8,
+        name: 'Onions',
+        price: 0.56,
+        image:
+        'https://www.instacart.com/image-server/394x394/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_ec4a5933-3593-468d-a1e8-946c8e9ceff0.jpg',
+        quantity: 0,
+      }
   ];
 
   export default quantites;
