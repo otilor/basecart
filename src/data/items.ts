84a7e7bea45d5ea639538daea1734600c84d5dc3
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2023-09-24 12:54:17 +0100

    update text and image

--- src/data/items.ts
+++ src/data/items.ts
@@ -9,7 +9,7 @@ export interface Item {
 const quantites: Item[] = [
     {
       id: 1,
-      name: 'Lemon',
+      name: 'Tomato',
       price: 2.33,
       image:
         'https://www.instacart.com/image-server/591x591/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_75c26b19-f902-4c53-9b78-b9efca722b13.jpg',
@@ -25,7 +25,7 @@ const quantites: Item[] = [
     },
     {
         id: 3,
-        name: 'Onions',
+        name: 'Bread',
         price: 0.56,
         image:
         'https://www.instacart.com/image-server/591x591/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_d7dcd978-645a-4e93-a073-a2dcba9be774.jpg',
@@ -33,7 +33,7 @@ const quantites: Item[] = [
       },
       {
         id: 4,
-        name: 'Onions',
+        name: 'Bell Pepper',
         price: 0.56,
         image:
         'https://www.instacart.com/image-server/591x591/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_18850fc6-5466-4c06-9d84-623509ffe430.jpg',
@@ -41,7 +41,7 @@ const quantites: Item[] = [
       },
       {
         id: 5,
-        name: 'Onions',
+        name: 'Lettuce',
         price: 0.56,
         image:
         'https://www.instacart.com/image-server/591x591/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_06c8d88f-3fea-4139-96ba-db2b41ac51d1.jpg',
@@ -49,7 +49,7 @@ const quantites: Item[] = [
       },
       {
         id: 6,
-        name: 'Onions',
+        name: 'Mushrooms',
         price: 0.56,
         image:
         'https://www.instacart.com/image-server/591x591/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_8ee9d1a8-988f-4d07-99be-57fb05a68767.jpg',
@@ -57,7 +57,7 @@ const quantites: Item[] = [
       },
       {
         id: 7,
-        name: 'Onions',
+        name: 'Brocolli',
         price: 0.56,
         image:
         'https://www.instacart.com/image-server/394x394/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_4453585c-c158-438d-bae5-980973efc462.jpg',
@@ -65,10 +65,10 @@ const quantites: Item[] = [
       },
       {
         id: 8,
-        name: 'Onions',
+        name: 'Lemon',
         price: 0.56,
         image:
-        'https://www.instacart.com/image-server/394x394/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_ec4a5933-3593-468d-a1e8-946c8e9ceff0.jpg',
+        'https://www.instacart.com/image-server/394x394/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_940d304a-6372-4c06-88ad-f006f547c9b9.jpg',
         quantity: 0,
       }
   ];
