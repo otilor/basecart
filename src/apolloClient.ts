26e06a9d2c11ac3c04170ee268a72f61aa4b50a0
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2023-09-25 13:17:45 +0100

    add apollo client

---
+++ src/apolloClient.ts
@@ -0,0 +1,9 @@
+// apolloClient.ts
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql", // Replace with your Apollo Server URL
+  cache: new InMemoryCache(),
+});
+
+export default client;
