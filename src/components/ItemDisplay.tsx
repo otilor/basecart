25065d358e5ea75736cc13e2aa5a80e73db81450
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2023-09-25 05:15:32 +0100

    let stepper update value

--- src/components/ItemDisplay.tsx
+++ src/components/ItemDisplay.tsx
@@ -4,11 +4,12 @@ import {Button} from 'baseui/button';
 import {FlexGridItem, FlexGrid} from 'baseui/flex-grid';
 import {Card, StyledBody, StyledAction} from 'baseui/card';
 import {Plus} from 'baseui/icon';
-import {Block, BlockProps} from 'baseui/block';
+import {BlockProps} from 'baseui/block';
+import {Stepper} from 'baseui/stepper';
 
 interface Props {
   items: Item[];
-  addToCart: (item: Item) => void;
+  addToCart: (item: Item, value: number) => void;
 }
 
 const itemProps: BlockProps = {
@@ -23,25 +24,38 @@ function ItemDisplay({items, addToCart}: Props) {
         flexGridColumnGap="scale800"
         flexGridRowGap="scale800"
       >
-        {items.map((element, index) => (
+        {items.map((item, index) => (
           <FlexGridItem key={index} {...itemProps}>
             <Card
               overrides={{Root: {style: {width: '328px'}}}}
-              headerImage={element.image}
-              title={element.name}
+              headerImage={item.image}
+              title={item.name}
             >
               <StyledBody>
                 Proin ut dui sed metus pharetra hend rerit vel non mi. Nulla
                 ornare faucibus ex, non facilisis nisl.
               </StyledBody>
               <StyledAction>
-                <Button
-                  startEnhancer={Plus}
-                  onClick={() => addToCart(element)}
-                  overrides={{BaseButton: {style: {width: '100%'}}}}
-                >
-                  Add to Cart
-                </Button>
+                {item.quantity < 1 && (
+                  <div>
+                    <Button
+                      startEnhancer={Plus}
+                      onClick={() => addToCart(item, 1)}
+                      overrides={{BaseButton: {style: {width: '100%'}}}}
+                    >
+                      Add to Cart
+                    </Button>
+                  </div>
+                )}
+
+                {item.quantity >= 1 && (
+                  <div>
+                    <Stepper
+                      value={item.quantity}
+                      setValue={(newValue) => addToCart(item, newValue)}
+                    />
+                  </div>
+                )}
               </StyledAction>
             </Card>
           </FlexGridItem>
